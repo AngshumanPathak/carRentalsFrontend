2331406dce5b96f779c79c759a9f136fd526caef
AngshumanPathak <pathakangshuman700@gmail.com>
2025-03-05 12:46:51 +0530

    av bar fix

--- src/components/shared/BottomNav.tsx
+++ src/components/shared/BottomNav.tsx
@@ -23,7 +23,7 @@ export const BottomNav = () => {
 
     return (
 
-        <div className="sm:hidden fixed bottom-2 left-60 transform -translate-x-1/2 flex space-x-4 bg-black/80 text-white p-1 shadow-lg z-50 w-100">
+        <div className="sm:hidden fixed bottom-2 left-55 transform -translate-x-1/2 flex space-x-4 bg-black/80 text-white p-1 shadow-lg z-50 w-100">
                 <Button className="bg-yellow-500 hover:bg-yellow-600 rounded-full px-4 py-2"><img src={home} alt="" className="w-10 p-2" onClick={() => handleScroll('home')}/></Button>
                 <Button className="bg-yellow-500 hover:bg-yellow-600 rounded-full px-4 py-2"><img src={about} alt="" className="w-10 p-2"onClick={() => handleScroll('about')}/></Button>
                 <Button className="bg-yellow-500 hover:bg-yellow-600 rounded-full px-4 py-2"><img src={services} alt="" className="w-10 p-2"onClick={() => handleScroll('search')}/></Button>
