95cd11d0151e689c69e9380fedc4e7ece5a2d684
AngshumanPathak <pathakangshuman700@gmail.com>
2026-02-09 12:56:50 +0530

    SEO changes5

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -21,10 +21,7 @@ const Home = () => {
     content="NH37 Car Rentals offers affordable car rentals in Guwahati with hatchbacks, sedans and SUVs. Easy booking for local and outstation trips in Assam."
   />
 
-  <meta
-    name="google-site-verification"
-    content="El8TrOc3-KmseD5W9VTtavT5dbjUgZV4FJ4SfM4nkAo"
-  />
+  
 </Helmet>
         
           <div id="home">
